06038524d9b8e0fab1fd2c168714e098061f5ea5
Derek Lomax <derekthecool@gmail.com>
2023-04-17 10:30:21 -0600

    Add neovim mason bin to path to get tools like prettier available in mark down files

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -4,17 +4,20 @@
 # [System.Console]::Title = "pwsh v$($PSVersionTable.PSVersion.ToString())"
 
 # Function to using my git bare repo for my windows config files
-function dot{git --git-dir="$env:USERPROFILE\.cfg" --work-tree="$env:USERPROFILE" $args}
+function dot
+{
+    git --git-dir="$env:USERPROFILE\.cfg" --work-tree="$env:USERPROFILE" $args
+}
 
 # Stop dotnet telemetry
 $env:DOTNET_CLI_TELEMETRY_OPTOUT = $true
 # PowerShell parameter completion shim for the dotnet CLI
 Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
-     param($commandName, $wordToComplete, $cursorPosition)
-         dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
-            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-         }
- }
+    param($commandName, $wordToComplete, $cursorPosition)
+    dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+    }
+}
 
 # Set PSReadLine Awesome Features
 # Set vi mode
@@ -27,31 +30,32 @@ $env:Path +=  ";$env:PROGRAMFILES\Open Steno Project\Plover 4.0.0.dev12\"
 $env:Path += ';C:\Windows\System32'
 $env:Path += ';C:\Program Files\Oracle\VirtualBox\'
 
+# Neovim setup
 Set-Alias 'v' 'nvim'
 
+## Add all these tools downloaded from neovim plugin Mason
+## https://github.com/williamboman/mason.nvim
+$mason_bin_path = "$env:LOCALAPPDATA\nvim-data\mason\bin"
+if(Test-Path $mason_bin_path)
+{
+    $env:Path += ";$mason_bin_path"
+}
+
 # Hack for running visual Studio for dotnet framework projects with terminal only
-function Enter-VS {
+function Enter-VS
+{
     C:\WINDOWS\SysWOW64\WindowsPowerShell\v1.0\powershell.exe -noe -c "&{Import-Module 'C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Microsoft.VisualStudio.DevShell.dll'; Enter-VsDevShell 0e7efad8}"
     Write-Host "Run msbuild -p:Configuration=Release"
 }
 
-# TODO: find a way to auto source scripts in a known directory 2023-03-09
-# Add additional folders to path variable to make it easier to call scripts
-# $env:Path += ";$env:USERPROFILE\Documents\WindowsPowerShell\Scripts\Wallaby"
-# $env:Path += ";$env:USERPROFILE\Documents\WindowsPowerShell\Scripts\CATM1"
-
-# $WallabyFunctionsScript = Join-Path -Path "$env:USERPROFILE\Documents\WindowsPowerShell" -ChildPath "Scripts\Wallaby\WallabyFunctions.ps1"
-# & "$WallabyFunctionsScript"
-#
-# $CATM1FunctionsScript = Join-Path -Path "$env:USERPROFILE\Documents\WindowsPowerShell" -ChildPath "Scripts\CAT1M1\CAT1M1-Functions.ps1"
-# & "$CATM1FunctionsScript"
-
 # Set Starship prompt
 # Support for OSC7 (CWD detector or wezterm terminal emulator)
 $prompt = ""
-function Invoke-Starship-PreCommand {
+function Invoke-Starship-PreCommand
+{
     $current_location = $executionContext.SessionState.Path.CurrentLocation
-    if ($current_location.Provider.Name -eq "FileSystem") {
+    if ($current_location.Provider.Name -eq "FileSystem")
+    {
         $ansi_escape = [char]27
         $provider_path = $current_location.ProviderPath -replace "\\", "/"
 
@@ -67,15 +71,17 @@ Invoke-Expression (&starship init powershell)
 # gh (GitHub CLI) completion
 Invoke-Expression -Command $(gh completion -s powershell | Out-String)
 
-function Update-Nvim_stimpack_config {
-Push-Location .
-cd $env:LOCALAPPDATA\nvim && git pull || Write-Host 'Failed to update' -ForegroundColor Red
-Write-Host 'Update success' -ForegroundColor Green
-Pop-Location
+function Update-Nvim_stimpack_config
+{
+    Push-Location .
+    cd $env:LOCALAPPDATA\nvim && git pull || Write-Host 'Failed to update' -ForegroundColor Red
+    Write-Host 'Update success' -ForegroundColor Green
+    Pop-Location
 }
 
 # This function sets all the build environment for ESP-IDF
-function Source-Espidf(){
+function Source-Espidf()
+{
     # Customize these for your install path and version
     $version = '5.0.1'
     $root_location = 'D:\'
@@ -84,20 +90,22 @@ function Source-Espidf(){
     $env:IDF_PATH = "${root_location}Espressif\frameworks\esp-idf-v${version}"
 
     # Run the setup script
-    Invoke-Expression "$root_location}Espressif/Initialize-Idf.ps1"
+    Invoke-Expression "${root_location}Espressif/Initialize-Idf.ps1"
 }
 
-function Get-TopProcesses {
+function Get-TopProcesses
+{
     get-process | Group-Object -Property ProcessName |
-    ForEach-Object {
-        [PSCustomObject]@{
-            ProcessName = $_.Name
-            Mem_MB = [math]::Round(($_.Group|Measure-Object WorkingSet64 -Sum).Sum / 1MB, 0)
-            ProcessCount = $_.Count
-        }
-    } | Sort-Object -desc Mem_MB | Select-Object -First 25
+        ForEach-Object {
+            [PSCustomObject]@{
+                ProcessName = $_.Name
+                Mem_MB = [math]::Round(($_.Group|Measure-Object WorkingSet64 -Sum).Sum / 1MB, 0)
+                ProcessCount = $_.Count
+            }
+        } | Sort-Object -desc Mem_MB | Select-Object -First 25
 }
 
-function rename_wezterm_title($title) {
-  echo "$([char]27)]1337;SetUserVar=panetitle=$([Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($title)))$([char]7)"
+function rename_wezterm_title($title)
+{
+    echo "$([char]27)]1337;SetUserVar=panetitle=$([Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($title)))$([char]7)"
 }
