5a3b426280ae2f00d8c7ba9094b460322995508d
Derek Lomax <derekthecool@gmail.com>
2024-04-03 12:12:00 -0600

    Change how pester before all looks for tests without using the home variable

--- MyCrossPlatformScripts/Tests/Setup-DotnetTools.Tests.ps1
+++ MyCrossPlatformScripts/Tests/Setup-DotnetTools.Tests.ps1
@@ -1,12 +1,20 @@
 BeforeAll {
-    $scriptName = [System.IO.Path]::GetFileNameWithoutExtension($PSCommandPath).Replace('.Tests', '')
-    $scriptPath = Get-ChildItem "$HOME/MyCrossPlatformScripts/" -Recurse -Filter "$scriptName.ps1"
-    | Where-Object FullName -NotMatch 'Tests'
-    | Select-Object -First 1 -ExpandProperty FullName
+    # Extract just the filename without extension and replace '.Tests' with nothing, assuming the test script ends with '.Tests.ps1'
+    $fileNameWithoutExtension = [System.IO.Path]::GetFileNameWithoutExtension($PSCommandPath)
+    $scriptName = $fileNameWithoutExtension -replace '\.Tests$', ''
+    $scriptFileName = "$scriptName.ps1"
+    $scriptBase = ([regex]::Match($PSCommandPath, '(.*[/\\]MyCrossPlatformScripts)')).Groups[1].Value
+
+    # Search for the script within the base directory, excluding any paths that still include 'Tests'
+    $scriptPath = Get-ChildItem -Path $scriptBase -Recurse -Filter $scriptFileName |
+        Where-Object { $_.FullName -notmatch 'Tests' } |
+        Select-Object -First 1 -ExpandProperty FullName
+
     if ($scriptPath) {
         . $scriptPath
     } else {
-        Write-Error "Expected script not found for: $scriptName"
+        Write-Error "Expected script not found for: $scriptFileName"
+        Write-Error "Script base: $scriptBase"
     }
 }
 
