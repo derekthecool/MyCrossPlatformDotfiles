40dcd405f4ed96708da545fa535351951ae11c83
Derek Lomax <derekthecool@gmail.com>
2024-04-09 10:33:42 -0600

    Update dot file cloning script

--- MyCrossPlatformScripts/PowershellTools/Clone-Repositories.ps1
+++ MyCrossPlatformScripts/PowershellTools/Clone-Repositories.ps1
@@ -1,8 +1,8 @@
-function Get-GitRepositoryConfiguration {
+function Clone-GitRepository {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true)]
-        [ValidatePattern('^(https://.*|ssh://.*|git@.*)$')]
+        [ValidatePattern('^(https://|ssh://|git@).*$')]
         [string]$RepoURL,
 
         [Parameter(Mandatory = $true)]
@@ -10,17 +10,25 @@ function Get-GitRepositoryConfiguration {
     )
 
     try {
+
         # Ensure the target directory exists, creating any necessary parent directories
-        if (-not (Test-Path -Path $TargetDirectory)) {
-            Write-Host "Creating target directory: $TargetDirectory"
-            New-Item -Path $TargetDirectory -ItemType Directory -Force | Out-Null
+        if (Test-Path -Path $TargetDirectory -ErrorAction SilentlyContinue)  {
+            if($(git rev-parse --is-inside-work-tree) -match 'true') {
+                Write-Host "Directory: $TargetDirectory is already a git repository" -ForegroundColor Green
+                return 'complete'
+            } else {
+                Write-Host 'Directory: $Target exists, but is not a git repository, exiting' -ForegroundColor Yellow
+                return 'check on existing files and try again'
+            }
         }
 
         # Clone the repository
-        Write-Host "Cloning repository from $RepoURL to $TargetDirectory"
-        git clone $RepoURL $TargetDirectory
+        Write-Host "Cloning repository $RepoURL to $TargetDirectory"
+        git clone --recurse-submodules "$RepoURL" "$TargetDirectory"
+        return 'complete'
     } catch {
         Write-Error "Failed to clone the repository. Error: $_"
+        return 'error'
     }
 }
 
@@ -35,8 +43,7 @@ function Get-NeovimConfiguration {
         $cloneTargetPath = Join-Path -Path ([Environment]::GetFolderPath([Environment+SpecialFolder]::ApplicationData)) -ChildPath 'nvim'
     }
 
-    New-Item -ItemType Directory "$HOME/.config" -ErrorAction SilentlyContinue
-    git clone "$RepoURL" "$cloneTargetPath"
+    return Clone-GitRepository -RepoURL "$RepoURL" -TargetDirectory "$cloneTargetPath"
 }
 
 # https://github.com/derekthecool/WeztermStimpack
@@ -44,12 +51,38 @@ function Get-WeztermConfiguration {
     [Parameter()]
     [string]$RepoURL = 'git@github.com:derekthecool/WeztermStimpack.git'
 
+    # This path is the same for windows and Linux
+    $cloneTargetPath = "$HOME/.config/wezterm"
+    return Clone-GitRepository -RepoURL "$RepoURL" -TargetDirectory "$cloneTargetPath"
+}
+
+# https://github.com/derekthecool/PloverStenoDictionaries
+function Get-PloverConfiguration {
+    [Parameter()]
+    [string]$RepoURL = 'git@github.com:derekthecool/PloverStenoDictionaries.git'
+
     if($IsWindows) {
-        $cloneTargetPath = Join-Path -Path ([Environment]::GetFolderPath([Environment+SpecialFolder]::LocalApplicationData)) -ChildPath 'nvim'
+        $cloneTargetPath = Join-Path -Path ([Environment]::GetFolderPath([Environment+SpecialFolder]::LocalApplicationData)) -ChildPath 'Plover'
     } else {
-        $cloneTargetPath = Join-Path -Path ([Environment]::GetFolderPath([Environment+SpecialFolder]::ApplicationData)) -ChildPath 'nvim'
+        $cloneTargetPath = Join-Path -Path ([Environment]::GetFolderPath([Environment+SpecialFolder]::ApplicationData)) -ChildPath 'Plover'
     }
 
-    New-Item -ItemType Directory "$HOME/.config" -ErrorAction SilentlyContinue
-    git clone "$RepoURL" "$cloneTargetPath"
+    return Clone-GitRepository -RepoURL "$RepoURL" -TargetDirectory "$cloneTargetPath"
+}
+
+# https://github.com/derekthecoolther/ExercismProgramming
+function Get-ExercismConfiguration {
+    [Parameter()]
+    [string]$RepoURL = 'git@github.com:derekthecool/ExercismProgramming.git'
+
+    # This path is the same for windows and Linux
+    $cloneTargetPath = "$HOME/Exercism"
+    return Clone-GitRepository -RepoURL "$RepoURL" -TargetDirectory "$cloneTargetPath"
+}
+
+function Get-AllConfigurations {
+    Get-NeovimConfiguration
+    Get-WeztermConfiguration
+    Get-PloverConfiguration
+    Get-ExercismConfiguration
 }
