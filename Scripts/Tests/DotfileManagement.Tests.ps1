d5fc8960d2e9d7bc3da5a8b94e18f4b35732e0ec
Derek Lomax <derekthecool@gmail.com>
2024-04-10 14:48:55 -0600

    Update module root for test files

--- Scripts/Tests/DotfileManagement.Tests.ps1
+++ Scripts/Tests/DotfileManagement.Tests.ps1
@@ -18,9 +18,27 @@ BeforeAll {
     }
 }
 
-Describe 'Mason tools in path' {
-    It 'Should add mason bin path to path environment variable' {
-        Add-MasonToolsToPath
-        {$env:Path -match 'mason'} | Should -Be $true
+Describe 'dot Tests' {
+    It 'Can call dot' {
+        { dot } | Should -Not -Throw
+    }
+
+    It 'Can call dot with an argument' {
+        { dot @('status') 2> $null } | Should -Not -Throw
+    }
+
+    It 'Calls git with any arguments' {
+        Mock git {}
+        dot 'status' 2> $null
+        Assert-MockCalled git -Exactly 1 -Scope It
+    }
+
+    It "Removes 'git' from the beginning of the arguments" {
+        # Mock git to intercept and inspect the call
+        Mock git {}
+
+        dot 'git' 'status' 2> $null
+        # Assert that git was called with 'status' only
+        Assert-MockCalled git -Exactly 1 -Scope It -ParameterFilter { $Args -contains 'status' -and $Args.Contains('git') -eq $false }
     }
 }
