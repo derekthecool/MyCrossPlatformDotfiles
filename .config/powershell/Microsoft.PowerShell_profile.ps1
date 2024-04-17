7e0a9a53fd735cfbe000626c831b1ef9a0ef1901
Derek Lomax <derekthecool@gmail.com>
2024-04-17 09:11:10 -0600

    Update Linux powershell profile to use HOME variable

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,3 @@
 # Dot source main powershell config stored in the windows style path from dotfiles
 # This is the powershell 7+ config using pwsh
-. ./Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+. "$HOME/Documents/PowerShell/Microsoft.PowerShell_profile.ps1"
