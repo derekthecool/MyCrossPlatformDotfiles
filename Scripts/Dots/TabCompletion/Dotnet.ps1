8c3f74a0b5766a082dda854fbce3e49e4cb793eb
Derek Lomax <derekthecool@gmail.com>
2024-05-30 20:52:38 -0600

    Add basic dotnet autocompletion help from Register-ArgumentCompleter help

---
+++ Scripts/Dots/TabCompletion/Dotnet.ps1
@@ -0,0 +1,7 @@
+$scriptblock = {
+    param($wordToComplete, $commandAst, $cursorPosition)
+    dotnet complete --position $cursorPosition $commandAst.ToString() | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+    }
+}
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock $scriptblock
