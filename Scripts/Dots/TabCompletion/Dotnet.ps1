5d9044c1b789342f21d66a43ed5916ab59286c89
Derek Lomax <derekthecool@gmail.com>
2024-05-30 21:07:46 -0600

    Update dotnet completion to sort items

--- Scripts/Dots/TabCompletion/Dotnet.ps1
+++ Scripts/Dots/TabCompletion/Dotnet.ps1
@@ -1,7 +1,42 @@
+<#
+This script started with the 3rd example from the Get-Help -All Register-ArgumentCompleter
+
+Then it has been enhanced to sort the completion items and move ones starting with symbols
+to the end of the list
+#>
 $scriptblock = {
     param($wordToComplete, $commandAst, $cursorPosition)
-    dotnet complete --position $cursorPosition $commandAst.ToString() | ForEach-Object {
+
+    # Capture the completion results
+    $completionResults = dotnet complete --position $cursorPosition $commandAst.ToString()
+
+    # Define a custom sort order function
+    function CustomSortOrder
+    {
+        param($item)
+        if ($item -match '^[a-zA-Z]')
+        {
+            return 0
+        } else
+        {
+            return 1
+        }
+    }
+
+    # Sort the completion results using the custom sort order function
+    $sortedResults = $completionResults
+    | Sort-Object @{
+        Expression = { CustomSortOrder $_ }
+        Ascending  = $true
+    }, @{
+        Expression = { $_ }
+        Ascending  = $true
+    }
+
+    # Create CompletionResult objects from the sorted results
+    $sortedResults | ForEach-Object {
         [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
     }
 }
+
 Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock $scriptblock
