bda1f14ae89f36752d03d0fed9db31ded4e2dc4b
Derek Lomax <derekthecool@gmail.com>
2024-06-29 16:49:35 -0600

    Update general-completion helper

--- Scripts/Dots/TabCompletion/General-Completion.ps1
+++ Scripts/Dots/TabCompletion/General-Completion.ps1
@@ -7,11 +7,14 @@ function Get-GeneralCompletion
 
     Process
     {
-        $helpOutput = Invoke-Expression "$Command --help"
+        $helpCommand = "$Command --help"
+        Write-Verbose "helpCommand:`n$helpCommand"
+        $helpOutput = Invoke-Expression "$helpCommand" 2>&1
+        Write-Verbose "helpOutput:`n$helpOutput"
         $helpOutput
         | Where-Object { -not ([string]::IsNullOrEmpty($_)) }
-        | ConvertFrom-Text -NoProgress '(?<CommandOrHelp>(-[a-zA-Z0-9-]+|--[a-zA-Z0-9-]+))'
-        | Sort-Object -Property CommandOrHelp -Unique
+        | ConvertFrom-Text -NoProgress '^\s{2,}(?<CommandOrHelp>(-[A-Za-z0-9]|-{2}[a-z][a-z0-9-]+))'
+        | Sort-Object -Property CommandOrHelp -Unique -CaseSensitive
     }
 }
 
