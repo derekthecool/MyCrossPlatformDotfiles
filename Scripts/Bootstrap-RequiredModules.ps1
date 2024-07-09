7d8c0e01d16c30071888679ca957ddc21a175dbf
Derek Lomax <derekthecool@gmail.com>
2024-07-09 15:11:24 -0600

    Create required module bootstrap installer for CI/CD

---
+++ Scripts/Bootstrap-RequiredModules.ps1
@@ -0,0 +1,17 @@
+Write-Output "Current location is: $PSScriptRoot"
+
+$modules = Get-ChildItem "$PSScriptRoot/Dots.psd1"
+| Select-String -AllMatches -Pattern "ModuleName\s*=\s*'(?<Name>[a-zA-Z0-9.-]+)';\s*ModuleVersion\s*=\s*'(?<Version>.*?)'"
+| Select-Object -ExpandProperty Matches
+| ForEach-Object {
+    $Name = $_.Groups['Name']
+    $Version = $_.Groups['Version']
+    [PSCustomObject]@{Name = $Name;Version = $Version}
+  }
+
+Write-Host "Modules found"
+$modules
+
+$modules | ForEach-Object {
+    Install-Module -Name $_.Name -MaximumVersion $_.Version
+}
