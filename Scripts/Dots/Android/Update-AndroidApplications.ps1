4e84c7dda508e40ddd287bf9bb08e1d0bac6b1b2
Derek Lomax <derekthecool@gmail.com>
2024-07-13 16:26:10 -0600

    Wrap android application file in a function

--- Scripts/Dots/Android/Update-AndroidApplications.ps1
+++ Scripts/Dots/Android/Update-AndroidApplications.ps1
@@ -1,4 +1,6 @@
-<#
+function Update-AndroidApplications
+{
+    <#
     .SYNOPSIS
     Return the full link to all website links on page
 
@@ -11,66 +13,68 @@
     .EXAMPLE
     PS> Get-WebsiteLinks -Website 'https://auroraoss.com/downloads/AuroraStore/Release/'
 #>
-function Get-WebsiteLinks {
-    param (
-        [Parameter()]
-        [string]$Website
+    function Get-WebsiteLinks
+    {
+        param (
+            [Parameter()]
+            [string]$Website
+        )
+
+        $SavedErrorAction = $ErrorActionPreference
+        $ErrorActionPreference = 'SilentlyContinue'
+
+        # Convert the string to a URI object
+        $uri = [System.Uri]::new($Website).GetLeftPart([System.UriPartial]::Authority)
+
+        Invoke-WebRequest $Website -UseBasicParsing
+        | Select-Object -ExpandProperty Links
+        | Select-Object -ExpandProperty href
+        | Where-Object { $_ -cmatch 'apk$' }
+        | ForEach-Object { "$uri/{0}" -f "$_" }
+
+        $SavedErrorAction = $ErrorActionPreference
+    }
+
+
+    # Add GitHub releases to download with gh here
+    $GitHubShortRepositoryNamesForGHDownload = @(
+        'TeamNewPipe/NewPipe'
     )
 
-    $SavedErrorAction = $ErrorActionPreference
-    $ErrorActionPreference = 'SilentlyContinue'
+    # Other apk downloads here
+    $DownloadFiles = @(
+        'https://f-droid.org/F-Droid.apk'
+        # Find latest somehow
+        # 'https://f-droid.org/repo/org.videolan.vlc_13050408.apk',
+        # 'https://auroraoss.com/AuroraStore/Stable/AuroraStore_4.3.0.apk'
+    )
 
-    # Convert the string to a URI object
-    $uri = [System.Uri]::new($Website).GetLeftPart([System.UriPartial]::Authority)
+    $AuroraStore = Get-WebsiteLinks -Website 'https://auroraoss.com/downloads/AuroraStore/Release/' | Select-Object -Last 1
+    $DownloadFiles += $AuroraStore
 
-    Invoke-WebRequest $Website -UseBasicParsing
-    | Select-Object -ExpandProperty Links
-    | Select-Object -ExpandProperty href
-    | Where-Object { $_ -cmatch 'apk$' }
-    | ForEach-Object { "$uri/{0}" -f "$_" }
+    $VLC = Get-WebsiteLinks -Website 'https://f-droid.org/en/packages/org.videolan.vlc/'
+    | Select-Object -Skip 1 -First 1
+    $DownloadFiles += $VLC
 
-    $SavedErrorAction = $ErrorActionPreference
+    # $GitHubShortRepositoryNamesForGHDownload | ForEach-Object {
+    #     $downloadURL = gh release --repo $_ view --json tagName,assets
+    #     | ConvertFrom-Json
+    #     | Select-Object -ExpandProperty assets
+    #     | Select-Object -ExpandProperty url
+    #
+    #     $DownloadFiles += $downloadURL
+    # }
+
+    $DownloadFiles
+
+    # $DownloadFiles = "$PSScriptRoot/apks"
+    # mkdir $DownloadFiles -ErrorAction SilentlyContinue
+    # Set-Location $DownloadFiles
+
+    # $DownloadFiles | ForEach-Object -ThrottleLimit 10 -Parallel {
+    #     $output_name = $_ -split '/' | Select-Object -Last 1
+    #     Invoke-WebRequest $_ -OutFile $output_name
+    # }
+
+    # Set-Location ..
 }
-
-
-# Add GitHub releases to download with gh here
-$GitHubShortRepositoryNamesForGHDownload = @(
-    'TeamNewPipe/NewPipe'
-)
-
-# Other apk downloads here
-$DownloadFiles = @(
-    'https://f-droid.org/F-Droid.apk'
-    # Find latest somehow
-    # 'https://f-droid.org/repo/org.videolan.vlc_13050408.apk',
-    # 'https://auroraoss.com/AuroraStore/Stable/AuroraStore_4.3.0.apk'
-)
-
-$AuroraStore = Get-WebsiteLinks -Website 'https://auroraoss.com/downloads/AuroraStore/Release/' | Select-Object -Last 1
-$DownloadFiles += $AuroraStore
-
-$VLC = Get-WebsiteLinks -Website 'https://f-droid.org/en/packages/org.videolan.vlc/'
-| Select-Object -Skip 1 -First 1
-$DownloadFiles += $VLC
-
-# $GitHubShortRepositoryNamesForGHDownload | ForEach-Object {
-#     $downloadURL = gh release --repo $_ view --json tagName,assets
-#     | ConvertFrom-Json
-#     | Select-Object -ExpandProperty assets
-#     | Select-Object -ExpandProperty url
-#
-#     $DownloadFiles += $downloadURL
-# }
-
-$DownloadFiles
-
-# $DownloadFiles = "$PSScriptRoot/apks"
-# mkdir $DownloadFiles -ErrorAction SilentlyContinue
-# Set-Location $DownloadFiles
-
-# $DownloadFiles | ForEach-Object -ThrottleLimit 10 -Parallel {
-#     $output_name = $_ -split '/' | Select-Object -Last 1
-#     Invoke-WebRequest $_ -OutFile $output_name
-# }
-
-# Set-Location ..
