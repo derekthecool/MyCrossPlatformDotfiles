c7ed383198f7d20e5d81f26ba36f0cfb1f75ecdf
Derek Lomax <derekthecool@gmail.com>
2024-10-16 11:06:56 -0600

    feat: add more format views

--- Scripts/Dots/Download-Commands.ps1
+++ Scripts/Dots/Download-Commands.ps1
@@ -1,17 +1,21 @@
-function Get-ISOFilename {
+function Get-ISOFilename
+{
     param (
         [Parameter()]
         [string]$Filename
     )
 
-    if($Filename -cmatch '([^/]+\.iso)') {
+    if($Filename -cmatch '([^/]+\.iso)')
+    {
         return $Matches[1]
-    } else {
+    } else
+    {
         throw 'The providing filename did not contain a ISO file match'
     }
 }
 
-function Download-ISO {
+function Download-ISO
+{
     param (
         [ValidatePattern('.*/([^/]+\.iso)')]
         [Parameter(Mandatory=$true)]
@@ -19,34 +23,40 @@ function Download-ISO {
 
         [string]$CheckSumLink,
 
-        [Parameter(Mandatory=$true)]
-        [ValidateSet('MD5', 'SHA1', 'SHA256', 'SHA384', 'SHA512')]
+        # [Parameter(Mandatory=$true)]
+        # [ValidateSet('MD5', 'SHA1', 'SHA256', 'SHA384', 'SHA512')]
         [string]$HashType
     )
 
     $LocalFilename = Get-ISOFilename -Filename $URL
 
-    # If a check sum link has been provided use it
-    # otherwise try to get it through sneaky approaches
-    if($CheckSumLink) {
-        $CheckSumText = [System.Text.Encoding]::UTF8.GetString($(Invoke-WebRequest -Uri $CheckSumLink | Select-Object -ExpandProperty Content))
-    } else {
-        Throw 'Could not download check sum file'
-    }
-
-
-    if(Test-Path $LocalFilename) {
+    if(Test-Path $LocalFilename)
+    {
         Write-Host "File: $LocalFilename exists locally, verifying the checksum"
-    } else {
+    } else
+    {
         Write-Host "Downloading ISO as local file: $LocalFilename"
         Invoke-WebRequest -Uri $URL -OutFile $LocalFilename
     }
 
+    # If a check sum link has been provided use it
+    # otherwise try to get it through sneaky approaches
+    if($CheckSumLink)
+    {
+        $CheckSumText = [System.Text.Encoding]::UTF8.GetString($(Invoke-WebRequest -Uri $CheckSumLink | Select-Object -ExpandProperty Content))
+    } else
+    {
+        Write-Error 'Could not download check sum file'
+        return
+    }
+
     $fileHash = Get-FileHash -Path $LocalFilename -Algorithm $HashType | Select-Object -ExpandProperty Hash
 
-    if($CheckSumText -match $fileHash) {
+    if($CheckSumText -match $fileHash)
+    {
         Write-Host 'ISO hash matched!' -ForegroundColor Green
-    } else {
+    } else
+    {
         Write-Host 'ISO hash did not match' -ForegroundColor Red
         Write-Host "Calculated hash: $fileHash"
         Write-Host "Downloaded hash data:`n$CheckSumText"
---
+++ Scripts/Dots/Formatting/PSDriveInfo.format.ps1
@@ -0,0 +1,19 @@
+# Uses EZOut to format as specified here
+# Generate by running ../Dots.EzFormat.ps1
+# Generated output located ../Dots.format.ps1xml
+Write-FormatView `
+    -TypeName 'System.Management.Automation.PSDriveInfo' `
+    -Name DotsRegexView `
+    -Property Name, Used, Root, Description `
+    -VirtualProperty @{
+    Used = {
+        if($_.Used -ne 0)
+        {
+            "{0}%" -f [Math]::Round(($_.Used / ($_.Used + $_.Free)) * 100, 0)
+        } else
+        {
+            0
+        }
+    }
+} `
+    -AutoSize
