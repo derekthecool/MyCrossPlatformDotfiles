08aa067b5ce11ab121c1771f0e87d9bf01cd6edd
Derek Lomax <derekthecool@gmail.com>
2025-02-20 08:10:50 -0700

    Delete most of the old bash scripts in .derek-shell-config
    perhaps later I can make a better way for some bash modules

--- .derek-shell-config/scripts/PullAdbPicturesToday.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pictureList=$(/mnt/c/WINDOWS/adb.exe shell "find /sdcard/DCIM/Camera/ ~/storage/*/DCIM/Camera/ -name $(date +"%Y%m%d")*.jpg")
-echo "$pictureList"
-
-for video in $pictureList; do
-	echo "Pulling image $video"
-	powershell.exe -NoProfile adb pull "$video"
-done
--- .derek-shell-config/scripts/lftp_launch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-lftp "$(fzf <~/.ftplist)"
