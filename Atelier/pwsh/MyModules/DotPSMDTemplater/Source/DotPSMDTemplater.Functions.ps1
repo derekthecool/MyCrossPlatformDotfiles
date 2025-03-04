317fdb091b0103688d13ccadf13e92b552d35000
Derek Lomax <derekthecool@gmail.com>
2025-03-04 14:21:30 -0700

    Improve function Update-DotPSMDTemplate to work with only my templates better

--- Atelier/pwsh/MyModules/DotPSMDTemplater/Source/DotPSMDTemplater.Functions.ps1
+++ Atelier/pwsh/MyModules/DotPSMDTemplater/Source/DotPSMDTemplater.Functions.ps1
@@ -5,6 +5,11 @@
 
 function Update-DotPSMDTemplate
 {
+    # Remove all templates before continuing
+    Get-DotPSMDTemplate | ForEach-Object {
+        Remove-PSMDTemplate -TemplateName $_.Name -Confirm:$false
+    }
+
     Get-ChildItem $HOME/Atelier/pwsh/PSModuleDevelopmentTemplates/ -Directory | ForEach-Object {
         Write-Verbose "Attempting to load directory: $_ as a PSMD project template"
         New-PSMDTemplate -ReferencePath $_.FullName
