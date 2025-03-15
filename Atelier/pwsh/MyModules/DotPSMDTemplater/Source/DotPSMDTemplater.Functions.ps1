19a30e4d260976fead8073bb3aefaafd320915cc
Derek Lomax <derekthecool@gmail.com>
2025-03-15 14:57:00 -0600

    Attempt to fix issues causing pester tests to fail in CI
    Change linux psmodulepath setup, and update DotPSMDTemplater

--- Atelier/pwsh/MyModules/DotPSMDTemplater/Source/DotPSMDTemplater.Functions.ps1
+++ Atelier/pwsh/MyModules/DotPSMDTemplater/Source/DotPSMDTemplater.Functions.ps1
@@ -1,16 +1,47 @@
+<#
+    .SYNOPSIS
+    Get the custom templates created in these Dotfiles
+
+    .DESCRIPTION
+    Get the custom templates created in these Dotfiles
+
+    .EXAMPLE
+    Get-DotPSMDTemplate
+#>
 function Get-DotPSMDTemplate
 {
     Get-PSMDTemplate -Store Default
 }
 
+<#
+    .SYNOPSIS
+    Delete all of my templates and load the latest version
+
+    .DESCRIPTION
+    This function makes Get-PSMDTemplate work the way I want it to with my templates
+
+    .PARAMETER Path
+    Optionally specify the path. Default is specified for local use already which references
+    the where all the templates are stored in this repository.
+    But to make this more testable via CI testing this parameter is supplied.
+    This repository is designed to be run as a git bare repository and testing
+    in a GH action assumes normal repository cloning.
+
+    .EXAMPLE
+    Update-DotPSMDTemplate
+#>
 function Update-DotPSMDTemplate
 {
+    param (
+        [string]$Path = "$HOME/Atelier/pwsh/PSModuleDevelopmentTemplates/"
+    )
+
     # Remove all templates before continuing
     Get-DotPSMDTemplate | ForEach-Object {
         Remove-PSMDTemplate -TemplateName $_.Name -Confirm:$false
     }
 
-    Get-ChildItem $HOME/Atelier/pwsh/PSModuleDevelopmentTemplates/ -Directory | ForEach-Object {
+    Get-ChildItem -Directory -Path $Path | ForEach-Object {
         Write-Verbose "Attempting to load directory: $_ as a PSMD project template"
         New-PSMDTemplate -ReferencePath $_.FullName
     }
--- Atelier/pwsh/MyModules/DotPSMDTemplater/Test/DotPSMDTemplater.Functions.Tests.ps1
+++ Atelier/pwsh/MyModules/DotPSMDTemplater/Test/DotPSMDTemplater.Functions.Tests.ps1
@@ -3,7 +3,12 @@ BeforeAll {
 }
 
 Describe 'DotPSMDTemplater tests' {
-    It 'Function Get-DotPSMDTemplate works' {
+    It 'Function Get-DotPSMDTemplate is null initially' {
+        Get-DotPSMDTemplate | Should -Be $null
+    }
+
+    It 'Function Get-DotPSMDTemplate is not null after loading my templates' {
+        Update-DotPSMDTemplate -Path "./Atelier/pwsh/PSModuleDevelopmentTemplates/"
         Get-DotPSMDTemplate | Should -Be -Not $null
     }
 }
