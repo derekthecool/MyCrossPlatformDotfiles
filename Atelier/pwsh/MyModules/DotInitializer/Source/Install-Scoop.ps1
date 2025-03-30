3fc397da30da7adfd09f1d055ec1bb343e890647
Derek Lomax <derekthecool@gmail.com>
2025-03-29 20:48:28 -0600

    DotInitializer fix add winget install

--- Atelier/pwsh/MyModules/DotInitializer/Source/Install-Scoop.ps1
+++ Atelier/pwsh/MyModules/DotInitializer/Source/Install-Scoop.ps1
@@ -7,7 +7,16 @@ function Install-Scoop
         # The only additional bucket for my needs
         scoop bucket add extras
 
-        # Required for extras bucket
-        scoop install git
+        # Install git if needed - this is needed for packages in the extras and versions buckets
+        if(-not $(Get-Command git -ErrorAction SilentlyContinue))
+        {
+            scoop install git
+        }
+
+        # Install winget if needed - it is not installed by default on CI servers
+        if(-not $(Get-Command winget -ErrorAction SilentlyContinue))
+        {
+            scoop install main/winget
+        }
     }
 }
