639a5895a411974b625604fea757a05c8373bada
Derek Lomax <derekthecool@gmail.com>
2025-04-18 15:18:12 -0600

    Fix dotinitializer

--- Atelier/pwsh/MyModules/DotInitializer/DotInitializer.psm1
+++ Atelier/pwsh/MyModules/DotInitializer/DotInitializer.psm1
@@ -15,7 +15,7 @@ if ($IsLinux -and ($PSVersionTable.OS -match 'Ubuntu'))
 if ($IsLinux -or $IsMacOS)
 {
     Install-Homebrew
-    Import-Module 'AnyPackage.Homebrew
+    Import-Module 'AnyPackage.Homebrew'
 }
 
 
