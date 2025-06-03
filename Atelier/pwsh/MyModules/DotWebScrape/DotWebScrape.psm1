b11212e4f23088241bb839f2b931fa095ebb71ba
Derek Lomax <derekthecool@gmail.com>
2025-06-03 13:42:22 -0600

    Update DotWebScrape to conditional build easy out formatters

--- Atelier/pwsh/MyModules/DotWebScrape/DotWebScrape.psm1
+++ Atelier/pwsh/MyModules/DotWebScrape/DotWebScrape.psm1
@@ -3,4 +3,17 @@
     . $_.FullName
 }
 
-Update-FormatData -PrependPath $PSScriptRoot/DotWebScrape.format.ps1xml
+$PSXmlFormatFile = Resolve-Path "$PSScriptRoot/*.format.ps1xml"
+if($PSXmlFormatFile)
+{
+    $EzOutBuildScript = Resolve-Path "$PSScriptRoot/*.EzFormat.ps1"
+    Write-Host "EzOutBuildScript: $($EzOutBuildScript.Path), env:DotEZOutBuildOnImport: $env:DotEZOutBuildOnImport"
+    if($EzOutBuildScript -and $env:DotEZOutBuildOnImport)
+    {
+        Write-Host "Building format files with ezout script: $($EzOutBuildScript.Path)"
+        & $EzOutBuildScript.Path
+    }
+
+    Write-Host "Loading $($PSXmlFormatFile.Path) format file"
+    Update-FormatData -PrependPath $PSXmlFormatFile.Path
+}
