b3278ff0f59f039c88a19e36df0a3bc16a7c1036
Derek Lomax <derekthecool@gmail.com>
2025-10-17 20:32:49 -0600

    Update Get-Tithing

--- Atelier/pwsh/MyModules/Dots/Church/Get-Tithing.ps1
+++ Atelier/pwsh/MyModules/Dots/Church/Get-Tithing.ps1
@@ -12,45 +12,49 @@
     This [string[]] variable allows for multiple filters for items to not match
 
     .EXAMPLE
-    $OutputTrasactions = Get-Tithing -TithingCsv ./oct262024_feb262025_tithing.csv -Filter tax, REIMBURSED
+    Get-Tithing -TithingCsv ./oct262024_feb262025_tithing.csv -Filter tax, REIMBURSED
 #>
 function Get-Tithing
 {
     param (
-        [Parameter(Mandatory)]
-        [ValidatePattern('\.csv$')]
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [ValidateScript({ Test-Path -Path $_ -PathType Leaf })]
         [string]$TithingCsv,
         [string[]]$AdditionalFilters
     )
 
-    if (-not (Test-Path $TithingCsv))
+    process
     {
-        throw "File $TithingCsv not found"
-    }
+        $exclude_patterns = @(
+            'JESUSCHRIST REIMBURSED'
+            'ATM DEPOSIT'
+            'ITEMS DEPOSITED'
+            'MOBILE BANKING FUNDS TRANSFER'
 
-    $exclude_patterns = @(
-        'JESUSCHRIST REIMBURSED'
-        'ATM DEPOSIT'
-        'ITEMS DEPOSITED'
-        'MOBILE BANKING FUNDS TRANSFER'
+            # Insert provided additional filters here
+            if ($AdditionalFilters)
+            {
+                $AdditionalFilters | ForEach-Object { $_ }
+            }
+        )
 
-        # Insert provided additional filters here
-        if ($AdditionalFilters)
-        {
-            $AdditionalFilters | ForEach-Object { $_ }
+        $transactions_raw = Import-Csv $TithingCsv | Where-Object { $_.Credit } | Select-Object Date, Credit, Description
+
+        $exclude_pattern = $exclude_patterns -join '|'
+        $transactions = $transactions_raw | Where-Object { $_.Description -notmatch $exclude_pattern }
+
+        $total = ($transactions | Measure-Object -Property Credit -Sum).Sum
+        $tithing = $total * 0.12
+
+        $transactions | Write-Host
+
+        # TODO: (Derek Lomax) 10/17/2025 8:31:19 PM, use a EZout format custom formatter here to show a nicer display!
+        [PSCustomObject]@{
+            Credit          = $total
+            Tithing         = $tithing
+            ExcludePatterns = $exclude_pattern
+            Path            = $TithingCsv
+            Transactions    = $transactions
         }
-    )
-
-    $transactions_raw = Import-Csv $TithingCsv | Where-Object { $_.Credit } | Select-Object Date, Credit, Description
-
-    $exclude_pattern = $exclude_patterns -join '|'
-    Write-Host "exclude_pattern: $exclude_pattern"
-    $transactions = $transactions_raw | Where-Object { $_.Description -notmatch $exclude_pattern }
-
-    $total = ($transactions | Measure-Object -Property Credit -Sum).Sum
-    $tithing = $total * 0.12
-    Write-Host "Total credit found: $($total.ToString('C'))" -ForegroundColor Green
-    Write-Host "Total tithing: $($tithing.ToString('C'))" -ForegroundColor Cyan
-
-    $transactions
+    }
 }
