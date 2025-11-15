5ccaba000c22b2a38d960689b3f68553554f87f2
Derek Lomax <derekthecool@gmail.com>
2025-11-15 16:45:45 -0700

    Add -ReloadClipboard param to DotClipboard

--- Atelier/pwsh/MyModules/DotClipboard/Source/DotClipboard.Functions.ps1
+++ Atelier/pwsh/MyModules/DotClipboard/Source/DotClipboard.Functions.ps1
@@ -5,6 +5,10 @@ function Get-ClipboardAsArray
         [Parameter(Position = 0, ParameterSetName = '__AllParameterSets')]
         [string]$Separator = "[`n`r`t, ]+",
 
+        [Parameter(ParameterSetName = '__AllParameterSets')]
+        [Alias('Reclip', 'R')]
+        [switch]$ReloadClipboard,
+
         [Parameter(ParameterSetName = 'Json')]
         [switch]$Json,
 
@@ -12,15 +16,17 @@ function Get-ClipboardAsArray
         [switch]$Describe,
 
         [Parameter(ParameterSetName = 'AsSqlQueryList')]
+        [Alias('asq', 'sql')]
         [switch]$AsSqlQueryList,
 
         [Parameter(ParameterSetName = 'AsSqlInsert')]
+        [Alias('asi', 'si', 'SqlInsert')]
         [switch]$AsSqlInsert
     )
 
     $items = (Get-Clipboard) -split $Separator | Where-Object { -not [string]::IsNullOrEmpty($_) }
 
-    switch ($PSCmdlet.ParameterSetName)
+    $result = switch ($PSCmdlet.ParameterSetName)
     {
         'Default' { $items }
         'Describe' { Write-Host "Count : $($items.Length)" -ForegroundColor Blue }
@@ -28,6 +34,12 @@ function Get-ClipboardAsArray
         'AsSqlQueryList' { ($items | ConvertTo-Json -Compress).TrimStart("[").TrimEnd("]") }
         'AsSqlInsert' { $items | ForEach-Object { "(`"$_`")" } | Join-String -Separator "," } 
     }
+
+    if ($ReloadClipboard)
+    {
+        $result | Set-Clipboard
+    }
+    $result
 }
 
 New-Alias -Name 'clipped' -Value Get-ClipboardAsArray
