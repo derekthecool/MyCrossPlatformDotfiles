e115229d283547b22a3b1502ef6b239195e04359
Derek Lomax <derekthecool@gmail.com>
2025-11-15 16:51:26 -0700

    Remove useless minimodule DotShowOff

--- Atelier/pwsh/MyModules/DotShowOff/DotShowOff.psm1
+++
@@ -1,4 +0,0 @@
-Get-ChildItem $PSScriptRoot/Source -Recurse -Filter '*.ps1' | ForEach-Object {
-    Write-Verbose "In $PSScriptRoot, sourcing file $_"
-    . $_.FullName
-}
--- Atelier/pwsh/MyModules/DotShowOff/Test/DotShowOff.Functions.Tests.ps1
+++
@@ -1,9 +0,0 @@
-BeforeAll {
-    Import-Module $PSScriptRoot/../*.psd1 -Force
-}
-
-Describe 'DotShowOff tests' {
-    It 'Function Show-Object works' {
-        Show-Object | Should -Be -Not $null
-    }
-}
