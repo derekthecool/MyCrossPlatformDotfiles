3d963e0e46b023837b7ca4687db087c425b0960e
Derek Lomax <derekthecool@gmail.com>
2025-12-01 10:07:37 -0700

    Fix container test string to bool error

--- Atelier/pwsh/MyModules/DotContainer/Test/DotContainer.Functions.Tests.ps1
+++ Atelier/pwsh/MyModules/DotContainer/Test/DotContainer.Functions.Tests.ps1
@@ -2,7 +2,7 @@
     Import-Module $PSScriptRoot/../*.psd1 -Force
 }
 
-Describe 'DotContainer tests' -Skip:($env:CI) {
+Describe 'DotContainer tests' -Skip:(-not [string]::IsNullOrEmpty($env:CI)) {
     It 'Function Get-ContainerRunner works' {
         Get-ContainerRunner | Should -Be -Not $null
     }
