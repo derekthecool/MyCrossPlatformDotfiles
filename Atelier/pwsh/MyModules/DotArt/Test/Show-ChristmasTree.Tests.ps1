b2de179e12cb45d40a5dead894ea81bfb6850ba1
Derek Lomax <derekthecool@gmail.com>
2026-01-08 11:57:55 -0700

    Do not test DotArt in CI

--- Atelier/pwsh/MyModules/DotArt/Test/Show-ChristmasTree.Tests.ps1
+++ Atelier/pwsh/MyModules/DotArt/Test/Show-ChristmasTree.Tests.ps1
@@ -2,7 +2,7 @@
     Import-Module $PSScriptRoot/../*.psd1 -Force
 }
 
-Describe 'DotArt tests' {
+Describe 'DotArt tests' -Skip:(-not(Test-Path Env:CI)) {
     It 'Function Show-ChristmasTree works' {
         Get-Command Show-ChristmasTree | Should -Be -Not $null
     }
