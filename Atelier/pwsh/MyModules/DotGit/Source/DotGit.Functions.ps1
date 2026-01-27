4d2569780d196ca91d7be7ea9f0ea79b49c78ad2
Derek Lomax <derekthecool@gmail.com>
2026-01-27 15:25:26 -0700

    feat: add new function Remove-GitWorktree

--- Atelier/pwsh/MyModules/DotGit/Source/DotGit.Functions.ps1
+++ Atelier/pwsh/MyModules/DotGit/Source/DotGit.Functions.ps1
@@ -1,9 +1,65 @@
+<#
+    .SYNOPSIS
+    Runs git worktree list and returns a rich object
+
+    .DESCRIPTION
+    Returns the result of git worktree list but also collects the date the
+    git worktree was last used. This useful for detecting which git worktrees
+    are old and should be purged.
+
+    .PARAMETER Name
+    Specifies the file name.
+
+    .EXAMPLE
+    Get-GitWorktree
+#>
 function Get-GitWorktree
 {
     [CmdletBinding()]
     [Alias('gwt')]
     param()
-    git worktree list | ConvertFrom-Text '(?<Path>\S+)\s+\w+\s\[(?<Name>\w+)\]'
+
+    git worktree list |
+    ConvertFrom-Text '(?<Path>\S+)\s+\w+\s\[(?<Name>\w+)\]' |
+    ForEach-Object {
+        $treeDirectory = (Get-Content "$($_.Path)/.git") -split ' ' | Select-Object -Last 1
+        $LastUsed = Get-ChildItem -Recurse -File $treeDirectory |
+            Sort-Object LastWriteTime |
+            Select-Object -Last 1 -ExpandProperty LastWriteTime
+    [PSCustomObject]@{
+            Path=$_.Path
+            Name=$_.Name
+            LastUsed=$LastUsed
+        }
+    } |
+    Sort-Object LastUsed
+}
+
+<#
+    .SYNOPSIS
+    Remove a git worktree
+
+    .DESCRIPTION
+    Takes input from Get-GitWorktree objects and will remove the git worktree.
+    If the git worktree is not clean e.g. unstaged files it will not be deleted.
+
+    .EXAMPLE
+    # Delete the first 5 git worktrees found (these will be the oldest and last used)
+    Get-GitWorktree | Select-Object -First 5 | Remove-GitWorktree
+
+    .EXAMPLE
+    # Delete all git worktrees that have not been used for the last 3 month
+    Get-GitWorktree | Where-Object { $_.LastUsed -lt (Get-Date).AddMonths(-3) } | Remove-GitWorktree
+#>
+filter Remove-GitWorktree
+{
+    [Alias('rwt')]
+    param (
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+        [string]$Name
+    )
+    Write-Verbose "Removing git worktree $Name"
+    & git worktree remove $Name
 }
 
 function Switch-GitWorktree
