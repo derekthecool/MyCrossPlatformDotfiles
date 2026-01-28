4efa708521f7c64f0eb97c6534447a8fc268a1c3
Derek Lomax <derekthecool@gmail.com>
2026-01-28 15:41:22 -0700

    fix: fix problem with DotfilesTests.ps1 reporting error falsely

--- DotfilesTests.ps1
+++ DotfilesTests.ps1
@@ -11,7 +11,9 @@ $config = New-PesterConfiguration -Hashtable @{
 }
 $results = Invoke-Pester -Configuration $config
 $results
-if ($results.Failed)
+if ($results.Failed -gt 0)
 {
     $results.Failed | Select-Object Name, Block, ErrorRecord | Format-List | Out-Host
+    exit 1
 }
+exit 0
