f7ec1c73cd4047f57af1a0f9e8a75d5629f9b1fe
Derek Lomax <derekthecool@gmail.com>
2026-02-01 08:47:18 -0700

    Add first python dotfile

---
+++ Atelier/python/scrape.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+import argparse
+
+parser = argparse.ArgumentParser("scrape")
+parser.add_argument("url", help="url to scrape", type=str) 
+args = parser.parse_args()
+print(args.url)
+exit(0)
+
+url = "https://pokemondb.net/pokedex/national"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# Example: Find all <a> tags with class 'ent-name'
+links = soup.find_all('a', class_='ent-name')
+for link in links:
+    print(*link.contents)
