147398861819a7a6be5ecababf989960d3609791
Derek Lomax <derekthecool@gmail.com>
2026-02-11 14:40:34 -0700

    fix: update test files that were not force imported modules

--- Atelier/pwsh/MyModules/DotBeautiful/Test/DotBeautiful.Functions.Tests.ps1
+++ Atelier/pwsh/MyModules/DotBeautiful/Test/DotBeautiful.Functions.Tests.ps1
@@ -1,6 +1,6 @@
 Describe 'DotBeautiful tests' {
     It 'No functions should be included' {
-        $functions = Import-Module $PSScriptRoot/../DotBeautiful.psd1 -PassThru
+        $functions = Import-Module $PSScriptRoot/../DotBeautiful.psd1 -Force -PassThru
         $functions.ExportedFunctions.Count | Should -Be 0
         $functions.ExportedAliases.Count | Should -Be 0
         $functions.ExportedCmdlets.Count | Should -Be 0
