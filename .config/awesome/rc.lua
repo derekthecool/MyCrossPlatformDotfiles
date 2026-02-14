12d5c99f92ba07db10c6ea7160c80206ebf398c2
Derek Lomax <derekthecool@gmail.com>
2026-02-13 21:36:31 -0700

    Work in progress adding minecraft windows to awesome window manager

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -619,6 +619,26 @@ awful.rules.rules = {
     },
 
     -- Floating clients.
+    --[[
+  minecraft windows
+  WM_ICON_NAME(STRING) = "Minecraft Launcher"
+_NET_WM_ICON_NAME(UTF8_STRING) = "Minecraft Launcher"
+WM_NAME(STRING) = "Minecraft Launcher"
+_NET_WM_NAME(UTF8_STRING) = "Minecraft Launcher"
+_NET_WM_ICON(CARDINAL) =
+_NET_WM_STATE(ATOM) =
+WM_CLASS(STRING) = "minecraft-launcher", "Minecraft Launcher"
+
+sign in window which looks really bad
+WM_ICON_NAME(STRING) = "Sign in to Minecraft"
+_NET_WM_ICON_NAME(UTF8_STRING) = "Sign in to Minecraft"
+WM_NAME(STRING) = "Sign in to Minecraft"
+_NET_WM_NAME(UTF8_STRING) = "Sign in to Minecraft"
+_NET_WM_ICON(CARDINAL) =
+_NET_WM_STATE(ATOM) =
+WM_CLASS(STRING) = "minecraft-launcher", "Minecraft Launcher"
+
+  --]]
     {
         rule_any = {
             instance = {
@@ -637,12 +657,14 @@ awful.rules.rules = {
                 'Wpa_gui',
                 'veromix',
                 'xtightvncviewer',
+                'minecraft-launcher',
             },
 
             -- Note that the name property shown in xprop might be set slightly after creation of the client
             -- and the name shown there might not match defined rules here.
             name = {
                 'Event Tester', -- xev.
+                -- 'Sign in to Minecraft',
             },
             role = {
                 'AlarmWindow', -- Thunderbird's calendar.
